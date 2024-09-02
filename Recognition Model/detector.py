f012cf0fcbf4abce3d45902bb25b9f2b378cc3d0
Rishit Chakraborty <125457882+recursioncat@users.noreply.github.com>
2024-09-03 01:31:19 +0530

    Update detector.py
    Takes the best of the two results

--- Recognition Model/detector.py
+++ Recognition Model/detector.py
@@ -6,6 +6,8 @@ import numpy as np
 app = Flask(__name__)
 
 class_names = class_names = ['1', '2', '3', '4', '5', '6','7','8','9', 'a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+model1 = keras.models.load_model('gesturesV7.keras')
+model2 = keras.models.load_model('gesturesV11.keras')
 
 def processImage(path):
     img = image.load_img(path, target_size=(128,128))
@@ -16,14 +18,24 @@ def processImage(path):
 
 @app.route('/predict', methods= ['POST'])
 def predict():
-    model = keras.models.load_model('gesturesV7.keras')
     data = request.get_json(force = True)
 
     url = data['url']
     image_arr = processImage(url)
-    prediction = model.predict(image_arr)
+    prediction1 = np.argmax(model1.predict(image_arr)[0])
+    prediction2 = np.argmax(model2.predict(image_arr)[0])
 
-    result = class_names[np.argmax(prediction[0])]
+    if abs(prediction1 - prediction2) < 5 or prediction1 == prediction2:
+        prediction = prediction2
+    
+    elif prediction1 > prediction2:
+        prediction = prediction2
+
+    elif prediction2 > prediction1:
+        prediction = prediction1
+    
+
+    result = class_names[prediction]
 
     return jsonify(result)
     
