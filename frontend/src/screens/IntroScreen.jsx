3062825dc2c2590f661ad596114505657e179234
sayan-majumder-github <sayanmajumder775@gmail.com>
2024-09-16 13:37:21 +0530

    Created a file named IntroScreen.jsx

---
+++ frontend/src/screens/IntroScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function IntroScreen() {
+  return (
+   <View className="bg-red-200">
+        <Text className="text-red-500">The Intro Screen</Text>
+   </View>
+  )
+}
