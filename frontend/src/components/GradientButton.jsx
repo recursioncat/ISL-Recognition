4e5e401d6e98bad8a69e7760ab32e537b77bf841
sayan-majumder-github <sayanmajumder775@gmail.com>
2024-09-18 00:53:22 +0530

    Created the Gradient Button

---
+++ frontend/src/components/GradientButton.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+const GradientButton = () => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button}>
+        <LinearGradient
+          colors={['#F4D03F', '#FADBD8']}
+          start={{ x: 0.0, y: 0.5 }}
+          end={{ x: 1.0, y: 0.5 }}
+          style={styles.gradient}
+        >
+          <Text style={styles.text}>Let's Get Started</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000', // For the dark background
+    marginBottom: 25,
+  },
+  button: {
+    width: 340, // Adjust width according to preference
+    borderColor: '#F4D03F', // Matching the start color of the gradient
+    borderWidth: 3, // 3px border width
+    overflow: 'hidden',
+    padding: 2 // Ensure gradient respects border radius
+  },
+  gradient: {
+    padding: 15, // Padding to increase button height and space around text
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#000', // Black text color
+    fontSize: 18, // Font size similar to the image
+    fontWeight: 'bold', // Bold font style
+  },
+});
+
+export default GradientButton;
