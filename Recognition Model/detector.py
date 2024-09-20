703db906eba99c3d7074d5ec40b8cdbae4b922f2
Rishit Chakraborty <125457882+recursioncat@users.noreply.github.com>
2024-09-20 15:06:21 +0530

    Update detector.py
    Added Functionality to download picture from the web

--- Recognition Model/detector.py
+++ Recognition Model/detector.py
@@ -1,17 +1,11 @@
 from flask import Flask, request, jsonify
-from tensorflow import keras
-import numpy as np
-from tensorflow.keras.preprocessing import image
+from dependencies import *
+import os
 
 app = Flask(__name__)
-model = keras.models.load_model('gesturesv13.keras')
+model = keras.models.load_model('gesturesv14.keras')
 class_names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s']
 
-def processImage(path):
-    img = image.load_img(path, target_size=(128, 128))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    return img_array
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -23,6 +17,17 @@ def predict():
 
     return jsonify(class_names[np.argmax(prediction)].lower())
 
+
+@app.route('/predictfromurl', methods=['GET','POST'])
+def predictfromurl():
+    data = request.get_json(force = True)
+    url = data['url']
+    download_image(url)
+    image_array = processImage('image.jpeg')
+    prediction = model.predict(image_array)
+    
+    return jsonify(class_names[np.argmax(prediction)].lower())
+    
 
 
 if __name__ == '__main__':
