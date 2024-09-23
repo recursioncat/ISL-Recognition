d3ff7ff6e5c0e385b388e7d4fbc9b9f31e3b2dac
Phinix-BI <biswajitdey.one@gmail.com>
2024-09-23 18:52:02 +0530

    Refactor ChatingNavigator and Tabs components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet,View} from 'react-native'
 import { Text } from 'react-native-paper'
 import { theme } from '../core/theme'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default function Header(props) {
-  return <Text style={styles.header} {...props} />
-}
 
-const styles = StyleSheet.create({
-  header: {
-    fontSize: 21,
-    color: theme.colors.primary,
-    fontWeight: 'bold',
-    paddingVertical: 12,
-  },
-})
+  return (
+  <View className="flex-row">
+    <Text className="text-2xl text-slate-200 font-bold p-5" >{props.title}</Text>
+    <View className="absolute right-0 py-5 px-2" >
+    {props.icon && (
+        <MaterialIcons
+          name={props.icon}
+          size={25}
+          color="#fff"
+          className="absolute right-0 p-5"
+        />
+      )}
+    </View>
+  </View>
+)}
+
+
