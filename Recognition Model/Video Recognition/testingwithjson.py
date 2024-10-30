6fd6d96765f8f7e07d17f5ad121120ed202675f1
Phinix-BI <biswajitdey.one@gmail.com>
2024-10-30 17:38:04 +0530

    Update model path in testingwithjson.py to absolute path

--- Recognition Model/Video Recognition/testingwithjson.py
+++ Recognition Model/Video Recognition/testingwithjson.py
@@ -5,7 +5,7 @@ import keras
 import os
 
 app = Flask(__name__)
-model = keras.models.load_model('VideoRecognitionV2_Bekar.keras')
+model = keras.models.load_model('C:\\Users\\Biswajit Dey\\Desktop\\ISL-Recognition\\Recognition Model\\Video Recognition\\VideoRecognitionV2_Bekar.keras')
 classes = ['Are you free today', 'Can you repeat that please',
        'Congratulations', 'help me please', 'how are you', 'I am fine',
        'I love you', 'no', 'Please come,Welcome', 'Talk slower please',
