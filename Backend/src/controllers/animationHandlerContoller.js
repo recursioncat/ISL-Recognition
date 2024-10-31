341ba3ba71798d0b2a7f30cc563d2416d940a881
Phinix-BI <biswajitdey.one@gmail.com>
2024-10-31 11:55:25 +0530

    Refactor animation request handler to improve error handling and add user authentication middleware

--- Backend/src/controllers/animationHandlerContoller.js
+++ Backend/src/controllers/animationHandlerContoller.js
@@ -1,42 +1,72 @@
 import responseHandler from "../utils/resHandler.js";
 import errorResponseHandler from "../utils/errorResponseHandler.js";
-import { textToIsl , speechToIsl , imgToIsl } from "../helpers/chatServicesHandler.js";
+import {
+    textToIsl,
+    speechToIsl,
+    imgToIsl,
+} from "../helpers/chatServicesHandler.js";
 
-export const handelAllRequests = async(req, res) => {
-
-    const {text , selectedService} = req.body;
+export const handelAllRequests = async (req, res) => {
+    const { text } = req.body;
     const file = req.file;
 
-    if(!text && !file){
-        return errorResponseHandler(res, 400, 'Please provide text or file');
+    if (!text && !file) {
+        return errorResponseHandler(res, 400, "Please provide text or file");
     }
 
-    if(!selectedService){
-        return errorResponseHandler(res, 400, 'Please provide selected service');
+    let selectedService;
+
+    if (text) {
+        selectedService = "textToIsl";
+    } else if (file) {
+        if (file.mimetype.startsWith("image")) {
+            selectedService = "imgToIsl";
+        } else if (file.mimetype.startsWith("audio")) {
+            selectedService = "speechToIsl";
+        } else {
+            return errorResponseHandler(res, 400, "Unsupported file type");
+        }
     }
 
-    try{
-
-        if(text){
-            if(selectedService === 'textToIsl'){
+    try {
+        if (text) {
+            if (selectedService === "textToIsl") {
                 const result = await textToIsl(text);
-                return responseHandler(res, 200, 'Response generated succesfully' , result);
+                return responseHandler(
+                    res,
+                    200,
+                    "Response generated succesfully",
+                    result
+                );
             }
         }
 
-        if(file){
-            if(selectedService === 'imgToIsl'){
+        if (file) {
+            if (selectedService === "imgToIsl") {
                 const result = await imgToIsl(file);
-                return responseHandler(res, 200, 'Response generated succesfully' ,result);
-            }else if(selectedService === 'speechToIsl'){
+                return responseHandler(
+                    res,
+                    200,
+                    "Response generated succesfully",
+                    result
+                );
+            } else if (selectedService === "speechToIsl") {
                 const result = await speechToIsl(file);
-                return responseHandler(res, 200, 'Response generated succesfully' ,result);
+                return responseHandler(
+                    res,
+                    200,
+                    "Response generated succesfully",
+                    result
+                );
             }
         }
 
-        return errorResponseHandler(res, 400, 'Unsupported service');
-    }catch(error){
-        return errorResponseHandler(res, 500, 'Error occured while processing request for service');
+        return errorResponseHandler(res, 400, "Unsupported service");
+    } catch (error) {
+        return errorResponseHandler(
+            res,
+            500,
+            "Error occured while processing request for service"
+        );
     }
-
-}
+};
